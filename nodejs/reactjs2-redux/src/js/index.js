5ac733f68114620279f0d1daac18e99ed02583bf
Morten Olav Hansen <mortenoh@gmail.com>
2017-09-15 09:29:37 +0700

    remove unused project reactjs2-redux

--- nodejs/reactjs2-redux/src/js/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Main from './Main';
-
-const renderApp = Component => {
-  render( <Component/>, document.getElementById( 'root' ) )
-};
-
-renderApp( Main );
